3b76e935b46e6f9310b714fc6dcbe36337d891d6
RazzDazz <magnus.wilde@hotmail.de>
2019-01-05 23:54:36 +0100

    Copy source from prom-repo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
+
 #!/bin/bash
+if [ ! -f "/opt/prometheus/prometheus.yml" ] 
+then
+    echo "Copying prometheus.yml.sample to prometheus.yml for first start to volume..." 
+    cp /tmp/prometheus.yml.sample /opt/prometheus/prometheus.yml
+fi
 
 echo "Starting supervisord to run prometheus ..." 
-exec /usr/bin/supervisord --nodaemon --configuration /tmp/supervisor_prometheus_nodeexp.conf
+exec /usr/bin/supervisord --nodaemon --configuration /tmp/supervisor_prometheus.conf
