c067d1ad116f6c241c2507abe5bb89dab704a776
RazzDazz <magnus.wilde@hotmail.de>
2019-01-06 09:12:30 +0100

    Correct typo n conf filename

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ then
 fi
 
 echo "Starting supervisord to run prometheus ..." 
-exec /usr/bin/supervisord --nodaemon --configuration /tmp/supervisor_prometheus_nodeexp.conf.conf
+exec /usr/bin/supervisord --nodaemon --configuration /tmp/supervisor_prometheus_nodeexp.conf
